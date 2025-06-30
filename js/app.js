bc25f57d2d8eb091cfffec2d7e86e18abbeba56d
Andy19955 <andy_19955@hotmail.com>
2025-06-30 03:20:42 +0200

    Add auth status check, display logged in / guest links based on auth status and is logged in / out redirects

--- js/app.js
+++ js/app.js
@@ -2,6 +2,11 @@ import { toggleMenu } from "./ui/shared/toggleMenu.js";
 import { registrationFormListener } from "./listeners/auth/registrationFormListener.js";
 import { loginFormListener } from "./listeners/auth/loginFormListener.js";
 import { logoutButtonListener } from "./listeners/auth/logoutButtonListener.js";
+import {
+  updateAuthUI,
+  redirectIfLoggedIn,
+  requireAuth,
+} from "./helpers/authStatus.js";
 
 /**
  * Routes the application based on current pathname.
@@ -20,17 +25,26 @@ function router() {
     case "/":
     case "/index.html":
       toggleMenu();
+      updateAuthUI();
       logoutButtonListener();
       break;
     case "/register/":
     case "/register/index.html":
+      redirectIfLoggedIn();
       toggleMenu();
       registrationFormListener();
       break;
     case "/login/":
     case "/login/index.html":
+      redirectIfLoggedIn();
       toggleMenu();
       loginFormListener();
+      break;
+    case "/profile/":
+    case "/profile/index.html":
+      requireAuth();
+      toggleMenu();
+      logoutButtonListener();
       break;
   }
 }
