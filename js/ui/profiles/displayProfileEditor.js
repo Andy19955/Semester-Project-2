6bc7f0a9b597fcf7191afcb342b39ec400667ac1
Andreas Thune <andy_19955@hotmail.com>
2025-08-27 20:20:55 +0200

    Add profile banner

--- js/ui/profiles/displayProfileEditor.js
+++ js/ui/profiles/displayProfileEditor.js
@@ -4,6 +4,9 @@
  *
  * @param {Object} profile - The profile object containing user information.
  * @param {string} profile.name - The name of the profile.
+ * @param {Object} profile.banner - The banner object containing the URL and alt text.
+ * @param {string} profile.banner.url - The URL of the banner image.
+ * @param {string} profile.banner.alt - The alt text for the banner image.
  * @param {Object} profile.avatar - The avatar object containing the URL and alt text.
  * @param {string} profile.avatar.url - The URL of the avatar image.
  * @param {string} [profile.bio] - The biography of the profile.
@@ -11,6 +14,10 @@
  * @example
  * const profile = {
  *   name: "John Doe",
+ *   banner: {
+ *     url: "https://example.com/banner.jpg",
+ *     alt: "John Doe's Banner"
+ *   },
  *   avatar: {
  *     url: "https://example.com/avatar.jpg",
  *     alt: "John Doe's Avatar"
@@ -24,8 +31,17 @@ export function displayProfileEditor(profile) {
   const profileName = document.querySelector("#profile-name");
   profileName.innerText = profile.name;
 
+  const profileBanner = document.querySelector("#bannerUrl");
+  profileBanner.value = profile.banner.url;
+
+  const profileBannerAlt = document.querySelector("#bannerAlt");
+  profileBannerAlt.value = profile.banner.alt;
+
   const profileAvatar = document.querySelector("#avatarUrl");
   profileAvatar.value = profile.avatar.url;
+
+  const profileAvatarAlt = document.querySelector("#avatarAlt");
+  profileAvatarAlt.value = profile.avatar.alt;
 
   const profileBio = document.querySelector("#bio");
   profileBio.innerText = profile.bio || "";
