082f1405d9a484aa86d902468e9d3d9ec23dbe69
Andreas Thune <andy_19955@hotmail.com>
2025-09-10 18:54:22 +0200

    Add support for localhost

--- js/helpers/getKey.js
+++ js/helpers/getKey.js
@@ -1,5 +1,15 @@
-export async function getKey() {
-  const response = await fetch("/.netlify/functions/getKeys");
-  const data = await response.json();
-  return data.apiKey;
+let getKey;
+
+if (window.location.hostname === "localhost") {
+  import("../config.js").then(({ LOCAL_API_KEY }) => {
+    getKey = () => LOCAL_API_KEY;
+  });
+} else {
+  getKey = async () => {
+    const response = await fetch("/.netlify/functions/getKeys");
+    if (!response.ok) throw new Error("Failed to fetch API key");
+    const data = await response.json();
+    return data.apiKey;
+  };
 }
+export { getKey };
