4f0666df020b04bd5e3224a4abe81d9ae0e6d680
Andreas Thune <andy_19955@hotmail.com>
2025-09-10 18:26:25 +0200

    Fix error in fetch url

--- js/helpers/getKey.js
+++ js/helpers/getKey.js
@@ -1,5 +1,5 @@
 export async function getKey() {
-  const response = await fetch("/netlify/functions/getKeys");
+  const response = await fetch("/.netlify/functions/getKeys");
   const data = await response.json();
   return data.apiKey;
 }
